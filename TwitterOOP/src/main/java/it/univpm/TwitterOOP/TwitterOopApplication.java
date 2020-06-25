44af02253ca98cf5a9dca0d2f1f45315a42a0e12
Davide Pacioni <s1086892@studenti.univpm.it>
2020-06-25 22:24:52 +0200

    add maven project

---
+++ TwitterOOP/src/main/java/it/univpm/TwitterOOP/TwitterOopApplication.java
@@ -0,0 +1,13 @@
+package it.univpm.TwitterOOP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterOopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterOopApplication.class, args);
+	}
+
+}
---
+++ TwitterOOP/src/test/java/it/univpm/TwitterOOP/TwitterOopApplicationTests.java
@@ -0,0 +1,13 @@
+package it.univpm.TwitterOOP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterOopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
