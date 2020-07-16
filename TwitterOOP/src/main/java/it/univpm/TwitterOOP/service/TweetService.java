37252d9ad6fbba220a0399bcd76e1a57594992e6
Davide Pacioni <s1086892@studenti.univpm.it>
2020-07-16 23:46:50 +0200

    added class Tweet Service and implemented

---
+++ TwitterOOP/src/main/java/it/univpm/TwitterOOP/service/TweetService.java
@@ -0,0 +1,22 @@
+package it.univpm.TwitterOOP.service;
+
+import java.util.Collection;
+
+import it.univpm.TwitterOOP.model.Metadata;
+import it.univpm.TwitterOOP.model.Tweet;
+
+/**
+ * Questa interfaccia utilizzata in TweetServiceImpl
+ * Viene utilizzata per specificare due metodi che permettono di visualizzare i metadata e la timeline
+ * @see TweetServiceImpl
+ * @see Tweet
+ * @see Metadata
+ * @author Davide Pacioni
+ * @author Alex Giaccio
+ *
+ */
+public interface TweetService {
+	
+	public abstract Collection<Tweet> getTweet();
+	public abstract Collection<Metadata> getMetada();
+}
