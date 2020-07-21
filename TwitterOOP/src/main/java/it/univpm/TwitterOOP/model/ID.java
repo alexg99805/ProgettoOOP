5d9a72da1ebc9bb6f51d4c8b35e096dd7e585eb2
Alex Giaccio <s1090636@studenti.univpm.it>
2020-07-21 23:33:14 +0200

    class Id

--- TwitterOOP/src/main/java/it/univpm/TwitterOOP/model/ID.java
+++ TwitterOOP/src/main/java/it/univpm/TwitterOOP/model/ID.java
@@ -1,5 +1,34 @@
 package it.univpm.TwitterOOP.model;
 
+/**
+ * Questa classe viene adoperata come classe suprema dato che ha una sola variabile che raffigura l'id 
+ * e che è collettivo alla maggioranza delle classi nel package model
+ * @author Davide Pacioni
+ * @author Alex Giaccio
+ *
+ */
 public class ID {
+	
+	private long id;
 
+
+	public ID() {
+		super();
+	}
+
+
+	/**
+	 * @return  id
+	 */
+	public long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id 
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+	
 }
