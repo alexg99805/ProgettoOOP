f1732a88dd0a252a852ab14a8ce4d88a3919ef49
Alex Giaccio <s1090636@studenti.univpm.it>
2020-07-22 16:16:40 +0200

    modified ArrayMetadata

--- TwitterOOP/src/main/java/it/univpm/TwitterOOP/service/ArrayMetadata.java
+++ TwitterOOP/src/main/java/it/univpm/TwitterOOP/service/ArrayMetadata.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import it.univpm.TwitterOOP.model.Metadata;
 
 /**
- * Questa classe ci permette di visualizzare un intero array contenente tutti i dettagli
+ * Questa classe permette di visualizzare un intero array contenente tutti i dettagli
  * riguardanti tutti i campi del tweet analizzato
  * @author Davide Pacioni
  * @author Alex Giaccio
@@ -21,7 +21,8 @@ public class ArrayMetadata {
 	 */
 	public static ArrayList<Metadata> getArrayMetadata() {
 		
-		metadata.add(new Metadata("created_at", "data e ora in cui il tweet Ã¨ stato pubblicato", "CreationDate"));
+		metadata.add(new Metadata("id","identificatore del tweet","Long"));
+		metadata.add(new Metadata("created_at", "data e ora in cui il tweet è stato pubblicato", "CreationDate"));
 		metadata.add(new Metadata("text","corpo del tweet","String"));
 		metadata.add(new Metadata("entities","ulteriori array in riferimento agli hashtag e alle immagini","ArrayList"));
 		metadata.add(new Metadata("hashtags","hashtag presenti nel tweet","ArrayList"));
