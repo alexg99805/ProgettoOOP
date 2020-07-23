093debbb172a84785c8677fd831d3caa33d86d91
Alex Giaccio <s1090636@studenti.univpm.it>
2020-07-23 16:06:48 +0200

    Modificata classe ArrayMetadata

--- TwitterOOP/src/main/java/it/univpm/TwitterOOP/service/ArrayMetadata.java
+++ TwitterOOP/src/main/java/it/univpm/TwitterOOP/service/ArrayMetadata.java
@@ -3,16 +3,26 @@ package it.univpm.TwitterOOP.service;
 import java.util.ArrayList;
 
 import it.univpm.TwitterOOP.model.Metadata;
+import it.univpm.TwitterOOP.model.Metadata;
+
+import java.util.ArrayList;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+ 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 /**
- * Questa classe permette di visualizzare un intero array contenente tutti i dettagli
+ * Questa classe ci permette di visualizzare un intero array contenente tutti i dettagli
  * riguardanti tutti i campi del tweet analizzato
  * @author Davide Pacioni
  * @author Alex Giaccio
  *
  */
 public class ArrayMetadata {
-	
 	private static ArrayList<Metadata> metadata = new ArrayList<Metadata>();
 	
 	/**
@@ -20,22 +30,29 @@ public class ArrayMetadata {
 	 * @return un array che contenente tutti i metadata necessari
 	 */
 	public static ArrayList<Metadata> getArrayMetadata() {
-		
-		metadata.add(new Metadata("id","identificatore del tweet","Long"));
-		metadata.add(new Metadata("created_at", "data e ora in cui il tweet � stato pubblicato", "CreationDate"));
-		metadata.add(new Metadata("text","corpo del tweet","String"));
-		metadata.add(new Metadata("entities","ulteriori array in riferimento agli hashtag e alle immagini","ArrayList"));
-		metadata.add(new Metadata("hashtags","hashtag presenti nel tweet","ArrayList"));
-		metadata.add(new Metadata("text","corpo dell'hashtag","String"));
-		metadata.add(new Metadata("image","contiene le informazioni relative all'immagine presente nel tweet","ArrayList"));
-		metadata.add(new Metadata("type","tipologia dell'immagine","String"));
-		metadata.add(new Metadata("size","misure riguardanti l'immagine","ArrayList"));
-		metadata.add(new Metadata("height","altezza immagine","Long"));
-		metadata.add(new Metadata("bytes","dimensione in byte riguardante l'immagine","Long"));
-		metadata.add(new Metadata("width","larghezza dell'immagine","Long"));
-		metadata.add(new Metadata("resize","ridimensionamento immagini senza modificarle","String"));
-		metadata.add(new Metadata("contaretw","num volte in cui il tweet è stato repostato dagli altri utenti","Long"));
-		metadata.add(new Metadata("lang","lingua in cui il tweet è stato twittato","String"));
-		return metadata;
+        JSONParser jsonParser = new JSONParser();
+         
+        try(FileReader reader = new FileReader("metadata.json"))
+        {
+            Object obj = jsonParser.parse(reader);
+            JSONArray file_metadata = (JSONArray) obj;
+           
+            //Iterate over employee array
+            file_metadata.forEach(single_metadata -> addMetadata((JSONObject) single_metadata));
+            return metadata;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+ 
+    private static void addMetadata(JSONObject single_metadata) 
+    {
+        Metadata x = new Metadata(single_metadata.get("field"), single_metadata.get("description"), single_metadata.get("var_type"));
+        metadata.add(x);
 	}
 }
